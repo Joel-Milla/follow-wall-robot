747466d413637ad863465c49f04495aa1c1063ca
Joel-Milla <jAlejandro.milla@outlook.com>
2025-07-25 14:16:20 +0200

    Finish adding the code for the action server to work.

--- ros2_ws/src/follow_wall/src/action_record_odom.cpp
+++ ros2_ws/src/follow_wall/src/action_record_odom.cpp
@@ -1,5 +1,4 @@
-#include "custom_messages/action/odom_record.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
+#include <cmath>
 #include <custom_messages/action/detail/odom_record__struct.hpp>
 #include <geometry_msgs/msg/detail/point32__struct.hpp>
 #include <memory>
@@ -7,6 +6,7 @@
 #include <nav_msgs/msg/detail/odometry__struct.hpp>
 #include <rclcpp/callback_group.hpp>
 #include <rclcpp/executors/multi_threaded_executor.hpp>
+#include <rclcpp/logger.hpp>
 #include <rclcpp/logging.hpp>
 #include <rclcpp/node.hpp>
 #include <rclcpp/node_options.hpp>
@@ -22,14 +22,25 @@
 
 struct DistanceTracker {
 private:
+  static constexpr float PROXIMITY_THRESHOLD = 0.2f;
+
   using Point = geometry_msgs::msg::Point32;
 
   std::mutex mutex_;
-  int num_laps_;
   std::vector<Point> checkpoints;
   Point current_pos;
+  int completed_laps_{0};
+  rclcpp::Logger logger = rclcpp::get_logger("distance_tracker");
+
+  float distance_between(const Point &point1, const Point &point2) {
+    float difference_x = (point1.x - point2.x);
+    float difference_y = (point1.y - point2.y);
+
+    return std::hypot(difference_x, difference_y);
+  }
 
 public:
+  int target_laps{0};
   /**
    * @brief Function called by the subscriber of main class that updates the
    * current odometry of robot
@@ -45,6 +56,57 @@ public:
     current_pos.set__y(new_y);
     current_pos.set__z(new_theta);
   }
+
+  /**
+   * @brief Save the current position of the robot in the vector 'checkpoints'
+   * that will be called from the action server every 'HZ_FREQUENCY'
+   *
+   * @return float of the traveled distance between the last known checkpoint
+   * and the current position of the robot
+   */
+  float create_checkpoint() {
+    std::lock_guard<std::mutex> lock{mutex_};
+    size_t num_checkpoints = checkpoints.size();
+    if (checkpoints.empty()) {
+      checkpoints.push_back(current_pos);
+      return 0.0f;
+    }
+
+    Point last_checkpoint = checkpoints[num_checkpoints - 1];
+    float distance_traveled = distance_between(last_checkpoint, current_pos);
+    RCLCPP_DEBUG(logger, "Distance between (%f, %f) and (%f, %f) is: %f",
+                 last_checkpoint.x, last_checkpoint.y, current_pos.x,
+                 current_pos.y, distance_traveled);
+
+    checkpoints.push_back(current_pos);
+
+    return distance_traveled;
+  }
+
+  /**
+   * @brief Checks if the robot is near its starting location. If true, means
+   * that has completed a lap, so update completed_laps_ variable
+   *
+   */
+  void update_laps() {
+    if (checkpoints.size() < 2)
+      return;
+
+    const Point &first_pos = checkpoints[0];
+    const Point &last_pos = checkpoints[checkpoints.size() - 1];
+
+    if (distance_between(first_pos, last_pos) <= PROXIMITY_THRESHOLD) {
+      completed_laps_++;
+      RCLCPP_DEBUG(logger, "Updated laps to: %i", completed_laps_);
+    }
+  }
+
+  bool is_finished() {
+    std::lock_guard<std::mutex> lock{mutex_};
+    return target_laps == completed_laps_;
+  }
+
+  std::vector<Point> get_checkpoints() { return checkpoints; }
 };
 
 class OdomTracker : public rclcpp::Node {
@@ -58,6 +120,7 @@ private:
   static constexpr const char *SUB_NAME = "odom";
   static constexpr const char *SRV_ACTION_NAME = "record_odom";
   static constexpr int GENERAL_QOS = 10;
+  static constexpr float HZ_FREQUENCY = (1.0f / 3.0f); //* Updates per second
 
   //* RCLCPP varaibles
   rclcpp_action::Server<OdomRecord>::SharedPtr srv_odom_;
@@ -76,6 +139,9 @@ private:
    * @param msg
    */
   void subscriber_callback(const OdomMsg::SharedPtr &msg) {
+    if (curr_state == IDLE)
+      return;
+
     auto point = msg->pose.pose.position;
     tracker_.update_position(point.x, point.y, point.z);
   }
@@ -104,13 +170,50 @@ private:
    * @param goal_handle
    */
   void handle_accepted(const std::shared_ptr<GoalHandle> &goal_handle) {
-    std::thread([this, &goal_handle]() {
-      this->execute(goal_handle);
-    }).detach();
+    std::thread([this, goal_handle]() { this->execute(goal_handle); }).detach();
   }
 
   void execute(const std::shared_ptr<GoalHandle> &goal_handle) {
-    (void)goal_handle;
+    RCLCPP_DEBUG(this->get_logger(), "Starting main execution");
+
+    curr_state = PROCESSING;
+    const auto goal = goal_handle->get_goal();
+    auto feedback = std::make_shared<OdomRecord::Feedback>();
+    auto result = std::make_shared<OdomRecord::Result>();
+    rclcpp::Rate loop_rate{HZ_FREQUENCY};
+
+    int number_laps = goal->num_laps;
+    tracker_.target_laps = number_laps;
+    float &distance_traveled = feedback->current_total;
+
+    //* While loop runs every HZ_FREQUENCY per second. Each time, it will create
+    // a 'checkpoint', saving the current position of the robot, and updating
+    // the total distance traveled by it
+    while (!tracker_.is_finished() && rclcpp::ok()) {
+      if (goal_handle->is_canceling()) {
+        result->list_of_odoms = tracker_.get_checkpoints();
+        goal_handle->canceled(result);
+        RCLCPP_DEBUG(this->get_logger(), "Goal canceled");
+        return;
+      }
+
+      distance_traveled += tracker_.create_checkpoint();
+      tracker_.update_laps();
+
+      goal_handle->publish_feedback(feedback);
+      RCLCPP_DEBUG(this->get_logger(), "Feedback published");
+
+      loop_rate.sleep();
+    }
+
+    if (rclcpp::ok()) {
+      result->list_of_odoms = tracker_.get_checkpoints();
+      goal_handle->succeed(result);
+      RCLCPP_DEBUG(this->get_logger(), "Goal succeed");
+    }
+
+    RCLCPP_DEBUG(this->get_logger(), "Finished execution");
+    curr_state = IDLE;
   }
 
 public:
@@ -125,7 +228,8 @@ public:
     sub_options.callback_group = sub_group;
     sub_odom_ = this->create_subscription<OdomMsg>(
         SUB_NAME, GENERAL_QOS,
-        [this](const OdomMsg::SharedPtr msg) { subscriber_callback(msg); });
+        [this](const OdomMsg::SharedPtr msg) { subscriber_callback(msg); },
+        sub_options);
 
     //* Create action
     this->srv_odom_ = rclcpp_action::create_server<OdomRecord>(
@@ -151,6 +255,9 @@ int main(int argc, char **argv) {
   auto _ = rcutils_logging_set_logger_level(action_srv->get_logger().get_name(),
                                             RCUTILS_LOG_SEVERITY_DEBUG);
   (void)_;
+  auto __ = rcutils_logging_set_logger_level("distance_tracker",
+                                             RCUTILS_LOG_SEVERITY_DEBUG);
+  (void)_, (void)__;
 
   rclcpp::executors::MultiThreadedExecutor executor;
   executor.add_node(action_srv);
